552be20fda4ccb7d67c39be3460f0e07b49937e2
Hien Le <hvan6@gatech.edu>
2018-08-23 23:14:48 -0700

    change weather app

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = 'a27cc9d72204d66bd80a8159fac26225';
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  //app.set('view engine', 'jade');
+  app.set('view engine','ejs')
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -27,8 +32,32 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+app.get('/', function (req, res) {
+  //res.send('Hello World!')
+  res.render('index', {weather: null, error: null});
+})
+
+//app.get('/users', user.list);
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
